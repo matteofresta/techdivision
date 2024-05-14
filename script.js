0bd1a65c6c3a5b5752f0b040cc2b23cd22d8c6db
matteofresta <matteofresta@hotmail.com>
2024-05-14 16:20:26 +0200

    Techdivision Projekt

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function showContent(sectionId) {
     // Hide all sections
-    var sections = document.querySelectorAll('.content-section');
+    let sections = document.querySelectorAll('.content-section');
     sections.forEach(function (section) {
         section.style.display = 'none';
 
     });
 
     // Show the selected section
-    var activeSection = document.getElementById(sectionId);
-    activeSection.style.display = 'block';
+    let activeSection = document.getElementById(sectionId);
+    activeSection.style.transition = '.5s ease';
+    activeSection.style.display = 'flex';
 }
